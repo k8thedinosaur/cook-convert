932d52219e4707372e948288cfaa2959d07c860f
Kate Farkas <kate.m.farkas@gmail.com>
2020-03-23 16:25:38 -0700

    write and test tablespoon/teaspoon functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,26 @@
-$(document).ready(function() {
+// $(document).ready(function() {
  
-})
+// })
+
+var measurement1 = prompt("What is your measurement?");
+
+// var teaspoonToTablespoon = function(measurement1) {
+//   var conversion = measurement1 / 3;
+//   return conversion;
+// };
+
+// console.log(measurement1 + " teaspoons = " + teaspoonToTablespoon(measurement1) + " tablespoons");
+
+
+var tablespoonToTeaspoon = function(measurement1) {
+  var conversion = measurement1 * 3;
+  return conversion;
+};
+
+console.log(measurement1 + " tablespoons = " + tablespoonToTeaspoon(measurement1) + " teaspoons");
+
+
+// var teaspoonToCup = function(measurement1) {
+//   var conversion = measurement * 48;
+//   return conversion;
+// }
